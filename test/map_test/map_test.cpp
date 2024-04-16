4ca81cc63a6efb1226ced142c134e576bd614f83
Fannil9527 <fannil9527@gmail.com>
2024-04-16 20:37:29 +0800

    add test

---
+++ test/map_test/map_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "pnc_map.h"
+using namespace std;
+
+void mapTest()
+{
+    cout << "This is pnc_map test" << endl;
+}
+
+int main()
+{
+    mapTest();
+    return 0;
+}
